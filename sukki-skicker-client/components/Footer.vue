8d16a98437d5c231972eee958abf6661348c472b
andmohiko <thomasito.berryz@gmail.com>
2020-09-23 22:31:19 +0900

    add mypage

---
+++ sukki-skicker-client/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="footer-container">
+    <div class="copyright">
+      <p>
+        <small>© 2020 @andmohiko</small>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.footer-container {
+  height: 80px;
+  background-color: #111;
+  display: flex;
+  padding: 33px;
+  justify-content: space-between;
+}
+
+.copyright {
+  display: flex;
+  justify-content: right;
+  align-items: center;
+
+  p {
+    line-height: 80px;
+    color: #ccc;
+    font-size: 12px;
+  }
+}
+</style>
