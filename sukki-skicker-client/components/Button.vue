0ae79d5a10099b27e5a7c82af3e4f805877c8cf0
andmohiko <thomasito.berryz@gmail.com>
2020-09-28 14:00:59 +0900

    add button component

---
+++ sukki-skicker-client/components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    type="button"
+    :style="[widthStyle]"
+    @click="$emit('onClick')"
+  >
+    {{ labelName }}
+  </button>
+</template>
+
+<script>
+
+export default {
+  props: {
+    labelName: {
+      type: String,
+      default: 'ボタン',
+      required: true
+    },
+    width: {
+      default: '100%',
+      type: String
+    }
+  },
+
+  data() {
+    return {
+      widthStyle: {
+        width: this.width
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
