4a8f78eec9e8300a985a0b35a3bc12cba406383a
andmohiko <thomasito.berryz@gmail.com>
2020-10-01 05:07:55 +0900

    add skicker seeds

--- sukki-skicker-server/db/seeds.rb
+++ sukki-skicker-server/db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Skicker.create!(
+  [
+    {
+      name: 'タップ',
+      power: 1,
+      cost: 0
+    },
+    {
+      name: 'ぺこぺこ',
+      power: 2,
+      cost: 15
+    },
+    {
+      name: 'ひもぺち',
+      power: 5,
+      cost: 30
+    },
+    {
+      name: 'はりせん',
+      power: 10,
+      cost: 80
+    },
+    {
+      name: 'クラッカー',
+      power: 30,
+      cost: 200
+    },
+    {
+      name: '湯呑み',
+      power: 50,
+      cost: 500
+    },
+    {
+      name: 'うに',
+      power: 80,
+      cost: 1000
+    },
+    {
+      name: 'カジキマグロ',
+      power: 160,
+      cost: 5000
+    },
+  ]
+)
