1df3d6f43ae3c7311d003c689fbf8ddb214352d5
andmohiko <thomasito.berryz@gmail.com>
2020-10-01 04:10:20 +0900

    add disabled button

--- sukki-skicker-client/components/Button.vue
+++ sukki-skicker-client/components/Button.vue
@@ -1,5 +1,16 @@
 <template>
   <button
+    v-if="isDisabled"
+    class="bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed"
+    type="button"
+    :style="[widthStyle]"
+    @click="$emit('onClick')"
+  >
+    {{ labelName }}
+  </button>
+
+  <button
+    v-else
     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
     type="button"
     :style="[widthStyle]"
@@ -14,13 +25,14 @@
 export default {
   props: {
     labelName: {
-      type: String,
       default: 'ボタン',
       required: true
     },
     width: {
       default: '100%',
-      type: String
+    },
+    isDisabled: {
+      default: false
     }
   },
 
